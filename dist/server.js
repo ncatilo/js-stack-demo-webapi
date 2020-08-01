d01d49e0e7c89482435254613947a57153d8299b
N Catilo <nrlc1971@gmail.com>
2020-08-01 10:07:23 +0800

    .

--- dist/server.js
+++ dist/server.js
@@ -1,16 +1,20 @@
 "use strict";
+// import express from 'express'
+// import * as _http from 'http'
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const app = require('http').createServer()
-var http_1 = __importDefault(require("http"));
-var app = http_1.default.createServer();
-var io = require('socket.io')(app);
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 var port = 3000;
+app.get('/', function (request, response) {
+    response.send('hello');
+});
 var SocketIOService_1 = __importDefault(require("./services/SocketIOService"));
 SocketIOService_1.default(io);
-app.listen(port, function () {
+http.listen(port, function () {
     console.log("Started on port " + port);
 });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
-// const app = require('http').createServer()
-import http from 'http'
-const app = http.createServer()
-const io = require('socket.io')(app)
+// import express from 'express'
+// import * as _http from 'http'
+
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 const port = 3000
+
+app.get('/', (request: any, response: any) => {
+    
+    response.send('hello')
+})
 
 import SocketIOService from './services/SocketIOService'
 
 SocketIOService(io)
 
-app.listen(port, () => {
+http.listen(port, () => {
 
     console.log(`Started on port ${port}`)
 });
