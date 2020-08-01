eccf834105dd195ae7dc4e3b1d5a5b1e97135618
N Catilo <nrlc1971@gmail.com>
2020-08-01 09:48:58 +0800

    .

--- dist/server.js
+++ dist/server.js
@@ -3,13 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var http = require('http').createServer();
-var io = require('socket.io')(http);
+var app = require('http').createServer();
+var io = require('socket.io')(app);
 var port = 3000;
 // require('../_aux/io')(io);
 var SocketIOService_1 = __importDefault(require("./services/SocketIOService"));
 SocketIOService_1.default(io);
-http.listen(port, function () {
+app.listen(port, function () {
     console.log("Started on port " + port);
 });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-const http = require('http').createServer()
-const io = require('socket.io')(http)
+const app = require('http').createServer()
+const io = require('socket.io')(app)
 const port = 3000
 
 // require('../_aux/io')(io);
@@ -7,7 +7,7 @@ import SocketIOService from './services/SocketIOService'
 
 SocketIOService(io)
 
-http.listen(port, () => {
+app.listen(port, () => {
 
     console.log(`Started on port ${port}`)
 });
